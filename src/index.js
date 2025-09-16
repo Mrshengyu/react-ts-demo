f5904b86ae9785bd44428875f1243acfd65c0e5f
shengyu <15022273097@163.com>
2025-09-16 21:25:49 +0800

    兼容antd

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+// 兼容包
+import '@ant-design/v5-patch-for-react-19';
+
 // 为提供的创建一个 React 根container并返回根。
 const root = createRoot(document.getElementById("root"));
 root.render(
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,58 +5,64 @@ import React, { Component } from 'react'
 import { Card, Form, Input, Button, message } from 'antd'
 import { useDispatch } from 'react-redux'
 import { fetchLogin } from '@/store/modules/user'
+import { useNavigate } from 'react-router-dom'
 
 
 
 
+export default function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [messageApi, contextHolder] = message.useMessage();
 
-export default function  Login() {
- const dispatch = useDispatch();
- const onFinish = values => {
+  const onFinish = async values => {
     console.log('Success:', values);
     //触发异步action fetchLogin
-    dispatch(fetchLogin(values));
-    message.success('登录成功');
+    //await dispatch(fetchLogin(values));
+    //跳转到首页
+    navigate('/')
+    // 提示用户
+    message.info('登录成功');
 
   };
-    return (
-      <div className="login">
-        <Card className="login-container">
-          <img src={logo} alt="" className="login-logo" />
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img src={logo} alt="" className="login-logo" />
 
-          {/* 登录表单 */}
-          <Form
-            onFinish={onFinish}
-            name="basic"
-            validateTrigger="onBlur"
+        {/* 登录表单 */}
+        <Form
+          onFinish={onFinish}
+          name="basic"
+          validateTrigger="onBlur"
 
+        >
+          <Form.Item
+            label="手机号"
+            name="phone"
+            rules={[
+              { required: true, message: 'Please input your phone!' },
+              { pattern: /^1[3-9]\d{9}$/, message: 'Please input correct phone number!' }
+            ]}
           >
-            <Form.Item
-              label="手机号"
-              name="phone"
-              rules={[
-                { required: true, message: 'Please input your phone!' },
-                { pattern: /^1[3-9]\d{9}$/, message: 'Please input correct phone number!' }
-              ]}
-            >
-              <Input placeholder="请输入手机号" size='large' />
-            </Form.Item>
-            <Form.Item
-              label="验证码"
-              name="cord"
-              rules={[{ required: true, message: 'Please input your cord!' }]}>
-              <Input placeholder="请输入验证码" size='large' />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType='submit' size='large' block>登录</Button>
-            </Form.Item>
-          </Form>
+            <Input placeholder="请输入手机号" size='large' />
+          </Form.Item>
+          <Form.Item
+            label="验证码"
+            name="cord"
+            rules={[{ required: true, message: 'Please input your cord!' }]}>
+            <Input placeholder="请输入验证码" size='large' />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType='submit' size='large' block>登录</Button>
+          </Form.Item>
+        </Form>
 
 
-        </Card>
-      </div>
-    )
-  
+      </Card>
+    </div>
+  )
+
 
 
 }
